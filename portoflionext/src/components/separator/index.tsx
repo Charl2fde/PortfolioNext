17af583080a09119e2de8d2eb8ec5b8778836a38
Chido Le Riz <charlesdefde@gmail.com>
2024-02-02 13:22:14 +0100

    📦 NEW: separator

---
+++ portoflionext/src/components/separator/index.tsx
@@ -0,0 +1,9 @@
+import styles from "@/styles/separator.module.css";
+
+const Separator = () => {
+    return (
+        <div className={styles.Separator}/>
+    );
+};
+
+export default Separator;
--- portoflionext/src/pages/index.tsx
+++ portoflionext/src/pages/index.tsx
@@ -1,8 +1,8 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import NavBar from "@/components/navbar";
+import Separator from "@/components/separator";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -38,6 +38,9 @@ export default function Home() {
               <p>Projets</p>
             </div>
           </div>
+        </section>
+        <section className={styles.containerApropos}>
+          <Separator />
         </section>
       </main>
     </>
